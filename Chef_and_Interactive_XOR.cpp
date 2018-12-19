9fa1817ac4339544641e4a9d7659508d143f49c0
Samarth Tandon <40452187+Samarth9798@users.noreply.github.com>
2018-12-19 19:49:28 +0530

    Update Chef_and_Interactive_XOR.cpp

--- Chef_and_Interactive_XOR.cpp
+++ Chef_and_Interactive_XOR.cpp
@@ -18,36 +18,35 @@ int main()
 	    i=1;j=2;k=3;l=4;
 	    if(n%4==0)
 	    {
-    	   while(l<=n)
-    	    {
-        	    cout<<1<<' '<<i<<' '<<j<<' '<<k<<endl<<flush;
-        	    cin>>abc;
-        	    cout<<1<<' '<<i<<' '<<j<<' '<<l<<endl<<flush;
-        	    cin>>abd;
-        	    cd=abc^abd;
-        	    cout<<1<<' '<<k<<' '<<l<<' '<<i<<endl<<flush;
-        	    cin>>cda;
-        	    a=cda^cd;
-        	    arr[i]=a;
-        	    cout<<1<<' '<<k<<' '<<l<<' '<<j<<endl<<flush;
-        	    cin>>cdb;
-        	    b=cdb^cd;
-        	    arr[j]=b;
-        	    ab=a^b;
-        	    c=abc^ab;
-        	    arr[k]=c;
-        	    d=abd^ab;
-        	    arr[l]=d;
-        	    i+=4;
-        	    j+=4;
-        	    k+=4;
-        	    l+=4;
-    	   }
+		   while(l<=n)
+		   {
+			    cout<<1<<' '<<i<<' '<<j<<' '<<k<<endl<<flush;
+			    cin>>abc;
+			    cout<<1<<' '<<i<<' '<<j<<' '<<l<<endl<<flush;
+			    cin>>abd;
+			    cd=abc^abd;
+			    cout<<1<<' '<<k<<' '<<l<<' '<<i<<endl<<flush;
+			    cin>>cda;
+			    a=cda^cd;
+			    arr[i]=a;
+			    cout<<1<<' '<<k<<' '<<l<<' '<<j<<endl<<flush;
+			    cin>>cdb;
+			    b=cdb^cd;
+			    arr[j]=b;
+			    ab=a^b;
+			    c=abc^ab;
+			    arr[k]=c;
+			    d=abd^ab;
+			    arr[l]=d;
+			    i+=4;
+			    j+=4;
+			    k+=4;
+			    l+=4;
+		   }
 	    }
 	    else
 	    {
-	        
-	            i=1;j=2;k=3;l=4;
+		    i=1;j=2;k=3;l=4;
 	            cout<<1<<' '<<i<<' '<<j<<' '<<k<<endl<<flush;
         	    cin>>abc;
         	    cout<<1<<' '<<i<<' '<<j<<' '<<l<<endl<<flush;
@@ -81,13 +80,13 @@ int main()
         	    arr[2]=b;
         	    arr[3]=c;
         	    arr[4]=d;
-        }
-	    cout<<2<<' ';
-	    for(i=1;i<=n;i++)
-	        cout<<arr[i]<<' ';
-	   cout<<endl<<flush;
-	   cin>>x;
-	   if(x==-1)
-	    break;
+        	}
+		cout<<2<<' ';
+		for(i=1;i<=n;i++)
+			cout<<arr[i]<<' ';
+		cout<<endl<<flush;
+		cin>>x;
+		if(x==-1)
+		    break;
 	}
 }
