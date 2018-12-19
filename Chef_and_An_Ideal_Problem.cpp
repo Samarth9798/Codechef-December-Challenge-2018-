195bf64284089d432294c1b3846496294acf6fa7
Samarth Tandon <40452187+Samarth9798@users.noreply.github.com>
2018-12-19 19:39:07 +0530

    Create Chef_and_An_Ideal_Problem.cpp

---
+++ Chef_and_An_Ideal_Problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y,z;
+	x=1;
+	cout<<x<<endl<<flush;
+	cin>>y;
+	if(y==x)
+	{
+	    z=2;
+	    cout<<z<<endl<<flush;
+	}
+	else if(y==2)
+	{
+	    z=3;
+	    cout<<z<<endl<<flush;
+	}
+	else if(y==3)
+	{
+	    z=2;
+	    cout<<z<<endl<<flush;
+	}
+}
